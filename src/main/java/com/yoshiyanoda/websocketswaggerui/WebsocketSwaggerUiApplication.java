b1415ebedfb78dfd84e2d1f4a040917429a76897
Yoshiya Noda <ny1425.0106@gmail.com>
2023-08-12 16:46:17 +0900

    initialize

---
+++ src/main/java/com/yoshiyanoda/websocketswaggerui/WebsocketSwaggerUiApplication.java
@@ -0,0 +1,13 @@
+package com.yoshiyanoda.websocketswaggerui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketSwaggerUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketSwaggerUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoshiyanoda/websocketswaggerui/WebsocketSwaggerUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoshiyanoda.websocketswaggerui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketSwaggerUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
